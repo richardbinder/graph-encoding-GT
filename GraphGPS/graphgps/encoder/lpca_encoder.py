2ba7d8305685b8c68fb2a9f073525e231bf140bb
Richard Binder <binder.richard@outlook.de>
2025-12-06 23:41:07 +0100

    peptides config changes

--- GraphGPS/graphgps/encoder/lpca_encoder.py
+++ GraphGPS/graphgps/encoder/lpca_encoder.py
@@ -18,37 +18,18 @@ class LPCAEncoder(torch.nn.Module):
 
         pecfg = cfg.posenc_LPCAEnc
         dim_pe = pecfg.dim_pe  # Size of Laplace PE embedding
-        model_type = pecfg.model  # Encoder NN model type for PEs
-        if model_type not in ['Transformer', 'DeepSet']:
-            raise ValueError(f"Unexpected PE model {model_type}")
-        self.model_type = model_type
         n_layers = pecfg.layers  # Num. layers in PE encoder model
         n_heads = pecfg.n_heads  # Num. attention heads in Trf PE encoder
 
-        # activation = nn.ReLU  # register.act_dict[cfg.gnn.act]
-        # if model_type == 'Transformer':
-        #     # Transformer model for LapPE
-        #     encoder_layer = nn.TransformerEncoderLayer(d_model=dim_pe,
-        #                                                nhead=n_heads,
-        #                                                batch_first=True)
-        #     self.pe_encoder = nn.TransformerEncoder(encoder_layer,
-        #                                             num_layers=n_layers)
-        # else:
-        #     # DeepSet model for LapPE
-        #     layers = []
-        #     if n_layers == 1:
-        #         layers.append(activation())
-        #     else:
-        #         self.linear_A = nn.Linear(dim_pe, 2 * dim_pe)
-        #         layers.append(activation())
-        #         for _ in range(n_layers - 2):
-        #             layers.append(nn.Linear(2 * dim_pe, 2 * dim_pe))
-        #             layers.append(activation())
-        #         layers.append(nn.Linear(2 * dim_pe, dim_pe))
-        #         layers.append(activation())
-        #     self.pe_encoder = nn.Sequential(*layers)
-
         self.pe_encoder = MLP(in_channels=dim_pe, hidden_channels=dim_pe*4, out_channels=dim_pe, num_layers=n_layers,
+                           dropout=cfg.posenc_LPCAEnc.dropout, norm=cfg.posenc_LPCAEnc.norm)
+
+        encoder_layer = nn.TransformerEncoderLayer(d_model=dim_pe*4,
+                                                   nhead=n_heads,
+                                                   batch_first=True)
+        self.pe_transformer = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
+
+        self.pe_encoder2 = MLP(in_channels=dim_pe*4, hidden_channels=dim_pe*8, out_channels=dim_pe, num_layers=n_layers,
                            dropout=cfg.posenc_LPCAEnc.dropout, norm=cfg.posenc_LPCAEnc.norm)
 
         self.expand_x = expand_x and self.emb_dim - self.enc_dim > 0
@@ -62,18 +43,10 @@ class LPCAEncoder(torch.nn.Module):
         lpca_enc = getattr(batch, 'lpca_enc')
 
         pos_enc = lpca_enc
-        # pos_enc = self.linear_A(pos_enc)  # (Num nodes) x dim_pe
 
-        # PE encoder: a Transformer or DeepSet model
-        if self.model_type == 'Transformer':
-            # pos_enc = self.pe_encoder(src=pos_enc,
-            #                           src_key_padding_mask=empty_mask[:, :, 0])
-            pass
-        else:
-            pos_enc = self.pe_encoder(pos_enc)
-
-        # Sum pooling
-        # pos_enc = torch.sum(pos_enc, 1, keepdim=False)  # (Num nodes) x dim_pe
+        pos_enc = self.pe_encoder(pos_enc)
+        # pos_enc = self.pe_transformer(pos_enc)
+        # pos_enc = self.pe_encoder2(pos_enc)
 
         if self.expand_x:
             h = self.linear_x(batch.x)
